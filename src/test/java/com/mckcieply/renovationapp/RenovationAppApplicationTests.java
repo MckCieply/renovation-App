e5a73d3a1b81f8c58d2e77bc33eff552012bd06d
Aleksander Torka <aleksander.torka@tech-com.pl>
2024-02-26 08:45:07 +0100

    Init

---
+++ src/test/java/com/mckcieply/renovationapp/RenovationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mckcieply.renovationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
