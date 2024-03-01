c8c2f8cf09e05de886ab209ca7ff87e216903dc1
Alex <mwppl123@gmail.com>
2024-03-02 00:34:49 +0100

    Contractors, Works, Work-Type init

---
+++ src/main/angular/src/app/work-type/work-type.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-type',
+  templateUrl: './work-type.component.html',
+  styleUrl: './work-type.component.scss'
+})
+export class WorkTypeComponent {
+
+}
