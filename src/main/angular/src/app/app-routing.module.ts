0e1bad5f0d54449158aa4d550823238a5f0aa622
Alex <mwppl123@gmail.com>
2024-03-02 00:35:56 +0100

    Room and Dashboard Schema
    Routing + Modules for new Components

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {RoomComponent} from "./room/room.component";
+import {WorksComponent} from "./works/works.component";
+import {ContractorsComponent} from "./contractors/contractors.component";
+import {WorkTypeComponent} from "./work-type/work-type.component";
 
 const routes: Routes = [
   { path: 'Dashboard', component: DashboardComponent },
+  { path: 'Works', component: WorksComponent },
+  { path: 'Contractors', component: ContractorsComponent },
+  { path: 'WorkType', component: WorkTypeComponent},
   { path: 'Room', component: RoomComponent },
 ];
 
--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RoomComponent } from './room/room.component';
+import { WorkTypeComponent } from './work-type/work-type.component';
+import { WorksComponent } from './works/works.component';
+import { ContractorsComponent } from './contractors/contractors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     DashboardComponent,
-    RoomComponent
+    RoomComponent,
+    WorkTypeComponent,
+    WorksComponent,
+    ContractorsComponent
   ],
   imports: [
     BrowserModule,
