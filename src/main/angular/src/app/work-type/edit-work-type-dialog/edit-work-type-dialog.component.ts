84e548cf790c14b38b89934ce40c8e6dc73b3ece
Alex <mwppl123@gmail.com>
2024-03-18 22:55:53 +0100

    Created add and edit dialogs for worktype

--- src/main/angular/src/app/work-type/edit-work-type-dialog/edit-work-type-dialog.component.ts
+++ src/main/angular/src/app/work-type/edit-work-type-dialog/edit-work-type-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {ErrorStateMatcher} from "@angular/material/core";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-edit-work-type-dialog',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class EditWorkTypeDialogComponent {
-
+  matcher = new ErrorStateMatcher();
+  constructor(public dialogRef: MatDialogRef<EditWorkTypeDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: {name: string, budgetPlanned: number}) {}
 }
