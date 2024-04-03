5b76655f1c678d87263e3fa794f77d7e0fb0ddee
Alex <mwppl123@gmail.com>
2024-04-03 22:33:25 +0200

    Secret Key Generator

--- src/main/java/com/mckcieply/renovationapp/RenovationAppApplication.java
+++ src/main/java/com/mckcieply/renovationapp/RenovationAppApplication.java
@@ -1,7 +1,11 @@
 package com.mckcieply.renovationapp;
 
+import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.security.Key;
 
 @SpringBootApplication
 public class RenovationAppApplication {
@@ -10,4 +14,18 @@ public class RenovationAppApplication {
 		SpringApplication.run(RenovationAppApplication.class, args);
 	}
 
+
+//	@Bean
+//	public static String secretKey() {
+//		// Generate a secret key
+//		Key key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
+//
+//		// Convert the key to a string
+//		String base64Key = java.util.Base64.getEncoder().encodeToString(key.getEncoded());
+//
+//		// Print the generated key
+//		System.out.println("Generated Secret Key: " + base64Key);
+//
+//		return base64Key;
+//	}
 }
