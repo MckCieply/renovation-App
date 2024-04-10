5e8948a7bfac90981664c1d4391ea9fff6317c80
Alex <mwppl123@gmail.com>
2024-04-10 16:11:59 +0200

    Add basic serverside validators

--- src/main/java/com/mckcieply/renovationapp/contractor/Contractor.java
+++ src/main/java/com/mckcieply/renovationapp/contractor/Contractor.java
@@ -2,6 +2,9 @@ package com.mckcieply.renovationapp.contractor;
 
 import com.mckcieply.renovationapp.enumerable.EnumContractorType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -13,17 +16,27 @@ public class Contractor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "First name is mandatory")
+    @Size(min=3, max = 100, message = "First name must be between 3 and 100 characters long")
     private String firstName;           // For private person or contactPerson for company
 
+    @NotBlank(message = "Last name is mandatory")
+    @Size(min=3, max = 100, message = "Last name must be between 3 and 100 characters long")
     private String lastName;
 
     @Transient
     private String fullName;
 
+    @NotBlank(message = "Email is mandatory")
+    @Size(min=3, max = 100, message = "Email must be between 3 and 100 characters long")
+    @Email(message = "Email should be valid")
     private String email;
 
+    @NotBlank(message = "Phone is mandatory")
+    @Size(min=8, max = 12, message = "Phone must be between 8 and 12 characters long")
     private String phone;
 
+    @NotBlank(message = "Type is mandatory")
     @Enumerated(EnumType.STRING)
     private EnumContractorType type;
 
--- src/main/java/com/mckcieply/renovationapp/workType/WorkType.java
+++ src/main/java/com/mckcieply/renovationapp/workType/WorkType.java
@@ -1,6 +1,8 @@
 package com.mckcieply.renovationapp.workType;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,6 +14,8 @@ public class WorkType {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is mandatory")
+    @Size(min=3, max = 100, message = "Name must be between 3 and 100 characters long")
     private String name;
 
 }
