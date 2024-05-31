0aec7f844fcaf8ab71cd9bcdc153ab3fb26e82b4
MckCieply <mwppl123@gmail.com>
2024-06-01 00:04:37 +0200

    Edited EnumRoles and created Role

--- src/main/java/com/mckcieply/renovationapp/auth/user/Role.java
+++ src/main/java/com/mckcieply/renovationapp/auth/user/Role.java
@@ -1,6 +1,31 @@
 package com.mckcieply.renovationapp.auth.user;
 
-public enum Role {
-    USER,
-    ADMIN
+import com.mckcieply.core.BaseEntity;
+import com.mckcieply.renovationapp.enumerable.EnumRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role extends BaseEntity {
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<AppUser> appUsers;
+
+    @Override
+    public void setName(String name) {
+        EnumRole role = EnumRole.fromString(name);
+        super.setName(role.toString());
+    }
 }
---
+++ src/main/java/com/mckcieply/renovationapp/enumerable/EnumRole.java
@@ -0,0 +1,16 @@
+package com.mckcieply.renovationapp.enumerable;
+
+public enum EnumRole {
+
+    USER,
+    ADMIN;
+
+    public static EnumRole fromString(String name){
+
+        for(EnumRole role: EnumRole.values()){
+            if(role.toString().equals(name))
+                return role;
+        }
+        throw new IllegalArgumentException("Invalid role name");
+    }
+}
