8c86581debda1b4cf7707a0973f6162dd6a6f696
MckCieply <mwppl123@gmail.com>
2024-11-08 22:58:12 +0100

    Adjusted getFiltered endpoint to auto assign params to fields in WorkFilter if name matches

--- src/main/java/com/mckcieply/renovationapp/work/WorkController.java
+++ src/main/java/com/mckcieply/renovationapp/work/WorkController.java
@@ -38,48 +38,16 @@ public class WorkController extends BaseController<Work, Long> {
     /**
      * Filters a list of {@link Work} entities based on various optional parameters provided as query parameters.
      *
-     * @param filter       the {@link WorkFilter} model attribute that will be populated with the provided filtering criteria
-     * @param daysCreated  optional number of days since creation to works
-     * @param daysUpdated  optional number of days since the last update to works
-     * @param createdBy    optional filter by the username of the creator
-     * @param updatedBy    optional filter by the username of the last updater
-     * @param name         optional filter by the name of the work
-     * @param state        optional filter by the {@link EnumWorkState} of the work
-     * @param paid         optional filter by the payment status of the work
-     * @param roomId       optional filter by the ID of the room associated with the work
-     * @param workTypeId   optional filter by the ID of the work type
-     * @param description  optional filter by a description, performing a case-insensitive match
+     * @param filter       the {@link WorkFilter} model attribute automatically populated with the query parameters if name is matched
      * @return             a {@link ResponseEntity} containing the filtered list of {@link Work} entities if available,
      *                     or a 204 No Content response if no matching records are found
      */
     @GetMapping("/filter")
-    public ResponseEntity<List<Work>> getFiltered(@ModelAttribute WorkFilter filter,
-                                                  @RequestParam(required = false) Integer daysCreated,
-                                                  @RequestParam(required = false) Integer daysUpdated,
-                                                  @RequestParam(required = false) String createdBy,
-                                                  @RequestParam(required = false) String updatedBy,
-                                                  @RequestParam(required = false) String name,
-                                                  @RequestParam(required = false) EnumWorkState state,
-                                                  @RequestParam(required = false) Boolean paid,
-                                                  @RequestParam(required = false) Long roomId,
-                                                  @RequestParam(required = false) Long workTypeId,
-                                                  @RequestParam(required = false) String description) {
-
-        filter.setDaysCreated(daysCreated);
-        filter.setDaysUpdated(daysUpdated);
-        filter.setName(name);
-        filter.setCreatedBy(createdBy);
-        filter.setUpdatedBy(updatedBy);
-        filter.setState(state);
-        filter.setPaid(paid);
-        filter.setRoomId(roomId);
-        filter.setWorkTypeId(workTypeId);
-        filter.setDescription(description);
+    public ResponseEntity<List<Work>> getFiltered(@ModelAttribute WorkFilter filter) {
 
         List<Work> filteredWorks = workService.getFiltered(filter);
-        if (filteredWorks.isEmpty()) {
+        if (filteredWorks.isEmpty())
             return ResponseEntity.noContent().build();
-        }
         return ResponseEntity.ok(filteredWorks);
     }
 }
