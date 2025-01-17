9a5b8e0675a8d7da76c8e99570250826175f67ea
MckCieply <mwppl123@gmail.com>
2025-01-17 13:24:11 +0100

    Fixed readability

--- src/main/angular/src/app/shared/services/theme.service.ts
+++ src/main/angular/src/app/shared/services/theme.service.ts
@@ -9,33 +9,19 @@ export class ThemeService {
   constructor() {
   }
 
-  /**
-   * Set the stylesheet with the specified key.
-   */
   setStyle(key: string, href: string) {
-    this.getLinkElementForKey(key).setAttribute( 'href', `${this.hrefCore}${href}`);
+    this.getLink(key).setAttribute( 'href', `${this.hrefCore}${href}`);
   }
 
-  /**
-   * Remove the stylesheet with the specified key.
-   */
-  removeStyle(key: string) {
-    const existingLinkElement = this.getExistingLinkElementByKey(key);
-    if (existingLinkElement) {
-      document.head.removeChild(existingLinkElement);
-    }
+  getLink(key: string) {
+    return this.getExistingLink(key) || this.createLink(key);
   }
 
-
-  getLinkElementForKey(key: string) {
-    return this.getExistingLinkElementByKey(key) || this.createLinkElementWithKey(key);
-  }
-
-  getExistingLinkElementByKey(key: string) {
+  getExistingLink(key: string) {
     return document.head.querySelector(`link[rel="stylesheet"].${key}`);
   }
 
-  createLinkElementWithKey(key: string) {
+  createLink(key: string) {
     const linkEl = document.createElement('link');
     linkEl.setAttribute('rel', 'stylesheet');
     linkEl.classList.add(key);
