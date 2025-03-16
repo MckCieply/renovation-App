ef06c93a58f99ec63b756769555e5f785ac7f0fe
Alex <mwppl123@gmail.com>
2025-03-16 21:31:35 +0100

    Dashboard charts init
    * Include @swimlane/ngx-charts dependency

--- src/main/angular/src/app/dashboard/dashboard.component.ts
+++ src/main/angular/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {BudgetService} from "../budget/budget.service";
+import {Budget} from "../shared/models/budget.model";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  totalBudget: any;
+  budgetChartData: any[] = [];
+  budgetColorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C']
+  }
 
+  budgetService = inject(BudgetService)
+
+  ngOnInit(): void {
+    this.budgetService.getBudget().subscribe(data => {
+      this.totalBudget = data;
+      this.budgetChartData = [
+        {
+          name: 'Budget Limit',
+          value: this.totalBudget.budgetLimit
+        },
+        {
+          name: 'Spent',
+          value: this.totalBudget.budgetSpent
+        },
+        {
+          name: 'Allocated',
+          value: this.totalBudget.budgetAllocated
+        }
+      ];
+    });
+  }
 }
