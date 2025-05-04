c3afb3869fdbdf73256a4272a34954050fc9df95
Alex <mwppl123@gmail.com>
2025-05-04 21:04:24 +0200

    resolves #24 refactor sidebar and app layout for dynamic expansion and improved styling

--- src/main/angular/src/app/shared/services/sidebar.service.ts
+++ src/main/angular/src/app/shared/services/sidebar.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SidebarService {
+  private _isSmallScreen$ = new BehaviorSubject<boolean>(false);
+  isSmallScreen$ = this._isSmallScreen$.asObservable();
+
   private _expanded$ = new BehaviorSubject<boolean>(false);
   isExpanded$ = this._expanded$.asObservable();
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe(result => {
+        this._isSmallScreen$.next(result.matches)
+      });
+  }
 
   toggleSidenav() {
     this._expanded$.next(!this._expanded$.value);
